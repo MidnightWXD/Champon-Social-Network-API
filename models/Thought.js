669002963ba8aa597f89b341b81b0a777f10f468
Yuchen Cui <yuchencui@hotmail.com>
2022-07-12 21:55:57 -0400

    set up app structure

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thought: {
+      type: String,
+      required: true,
+      //must between 1 and 280 characters
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //use a getter to format the date on query
+      get: (createdAtVal) => {
+        return createdAtVal.toLocaleDateString();
+      }
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Student = model('student', thoughtSchema);
+
+module.exports = Student;
---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    courseName: {
+      type: String,
+      unique: true,
+      required: true,
+      Trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      //validate email format
+      validate: [validateEmail, 'Please fill a valid email address'],
+      //match email format by using regex
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  //retrieve the length of the user's friends array field on query
+  return this.friends.length;
+});
+
+const User = model('course', userSchema);
+
+module.exports = User;
